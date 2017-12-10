200619cf34b5f29785af5ae9048a1326dc306a7c
roni <roni@github.com>
2017-12-11 00:53:42 +0600

    add dashboard

---
+++ view/mainView/dashboard.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Contact Us</title>
+</head>
+<body>
+<font size="2" face="Consolas">
+    <table action="#"  width="95%" align="center">
+        <?php include("../partialView/top.php"); ?>
+        <tr>
+            <td>
+                <table>
+                    <tr>
+                        <?php include("../partialView/nav.php"); ?>
+                        <td bgcolor="WhiteSmoke" width="80%" valign="top">
+                            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <font size="6" color="LightSalmon   "><b> Md Tuser Sheikh </b></font>
+                            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <font size="4" color="SlateGrey" > Welcome to <b> Bookstore </b></font>
+                            <br><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <font size="3" color="SlateGrey" > User Since : <b> 2 years back </b></font>
+                            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <font size="3" color="SlateGrey" > Last Login : <b> 5 days ago </b></font>
+
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <?php include("../partialView/bottom.php"); ?>
+    </table>
+</font>
+</body>
+</html>
