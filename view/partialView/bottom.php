dfb23877ee22391226e8d1b43f7df74b44d81b51
Md Tuser Sheikh <tsheikh21@live.com>
2017-12-11 01:33:35 +0600

    add bottom bar

---
+++ view/partialView/bottom.php
@@ -0,0 +1,11 @@
+<tr bgcolor="WhiteSmoke" height="40px">
+    <td align="center">
+        <font color="SlateGrey"> Copyright&copy; 2017 - Book Store </font>
+    </td>
+</tr>
+
+<script>
+    var x = document.getElementsByTagName("A");
+    for (var i = 0; i < x.length; i++)
+        x[i].style.textDecoration = "none";
+</script>
