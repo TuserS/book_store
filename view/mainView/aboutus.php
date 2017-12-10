79ed900bd69898534fc2418c829dc63b612a14e6
shaira22 <33633145+shaira22@users.noreply.github.com>
2017-12-11 01:11:10 +0600

    AboutUS  Autors Boimela Categories Added

---
+++ view/mainView/aboutus.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <title>About Us</title>
+</head>
+<body>
+<font size="2" face="Consolas">
+    <table action="#"  width="95%" align="center">
+        <?php include("../partialView/top.php"); ?>
+
+        <tr height="500" bgcolor="WhiteSmoke">
+            <td valign="top" align='center'> <br><br>
+                <table width="70%" align="center">
+                    <td align="left">
+                        <p align="center"><font size="6" face="Consolas" color="SLATEBLUE"><b>About Us</b></font></p>
+                        <br>
+                        <p align="justify"><font color="LightSlateGray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae fuga at exercitationem dolore dicta deleniti.
+                        Voluptas minima magni perferendis suscipit velit voluptates dolore, amet tenetur qui hic rerum ea minus asperiores
+                        tempora culpa reprehenderit nostrum quo. Eligendi at nulla sunt quod expedita tempora alias. Voluptas earum repellat
+                        nulla consequuntur facilis!</font>
+                        </p>
+                        <font size="5" face="Consolas" color="SLATEBLUE"><b>Mission</b></font>
+                        <p align="justify"><font color="LightSlateGray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae fuga at exercitationem dolore dicta deleniti.
+                        Voluptas minima magni perferendis suscipit velit voluptates dolore, amet tenetur qui hic rerum ea minus asperiores
+                        tempora culpa reprehenderit nostrum quo. Eligendi at nulla sunt quod expedita tempora alias. Voluptas earum repellat
+                        nulla consequuntur facilis!</font>
+                        </p>
+                        <font size="5" face="Consolas" color="SLATEBLUE"><b>Vision</b></font>
+                        <p align="justify"><font color="LightSlateGray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae fuga at exercitationem dolore dicta deleniti.
+                        Voluptas minima magni perferendis suscipit velit voluptates dolore, amet tenetur qui hic rerum ea minus asperiores
+                        tempora culpa reprehenderit nostrum quo. Eligendi at nulla sunt quod expedita tempora alias. Voluptas earum repellat
+                        nulla consequuntur facilis!</font>
+                        </p>
+
+                    </td>
+                </table>
+            </td>
+        </tr>
+
+        <?php include("../partialView/bottom.php"); ?>
+    </table>
+</font>
+</body>
+</html>
---
+++ view/mainView/boimela.php
@@ -0,0 +1,236 @@
+<html>
+<head>
+    <title>Categories</title>
+</head>
+<body>
+<font size="2" face="Consolas" >
+    <table action="#"  width="95%" align="center">
+        <?php include("../partialView/top.php"); ?>
+
+
+
+        <tr height="500" bgcolor="WhiteSmoke">
+
+            <td align="center">
+
+                <br><br>
+                <font size="6" face="Consolas" color="SLATEBLUE"><b>Boimela 2017</b></font>
+                <br>
+
+                <table align="center" width="80%" cellspacing="20px" cellpadding="10px">
+                    <tr height="60px">
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+
+
+                    </tr>
+                    <tr height="60px">
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+
+
+                    </tr>
+                    <tr height="60px">
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+
+
+                    </tr>
+                    <tr height="60px">
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+
+
+                    </tr>
+                    <tr height="60px">
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd">
+                            <a href="#">
+                                <img src="../../img/book/book1.jpg" width="100px"> <br>
+                                <hr width="50%">
+                                <font color="DodgerBlue" ><b>145 Tk</b></font>
+                                <hr width="50%">
+                                <font color="#555" size="3"><b>Muhammed Zafar Iqbal</b></font>
+                            </a>
+                        </td>
+
+
+                    </tr>
+
+
+
+
+                </table>
+                <br><br>
+            </td>
+        </tr>
+
+
+        <?php include("../partialView/bottom.php"); ?>
+    </table>
+</font>
+
+</body>
+</html>
