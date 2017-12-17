fee97b12ad07f5489772207a33efeb4f6ddf04ac
Md Tuser Sheikh <tsheikh21@live.com>
2017-12-17 21:46:18 +0600

    category and advance search updated

--- view/mainView/categories.php
+++ view/mainView/categories.php
@@ -4,32 +4,6 @@
 </head>
 <body>
 
-  <script>
-			
-			  
-			  function searchCategory() {
-				  td[i].style.display = "table-cell";
-				 <!-- var input, filter, table, tr, td, i;
-				  input = document.getElementById("categorySearch");
-				  filter = input.value.toUpperCase();
-				  table = document.getElementById("categoryTable");
-				  tr = document.getElementsByTagName("tr");
-				  td = document.getElementsByTagName("td");
-				 // b=table.getElementsByTagName("b");
-				  for (i = 0; i < td.length; i++) {
-					//td = tr[i].getElementsByTagName("td")[0];
-					b=td[i].getElementByTagName("b")[0];
-					if (b) {
-					  if (b.innerHTML.toUpperCase().indexOf(filter) > -1) {
-						td[i].style.display = "table-cell";
-					  } else {
-						td[i].style.display = "table-cell";
-					  }
-					}       
-				  }
-				}
-	 </script
-
 <font size="2" face="Consolas" >
     <table action="#"  width="95%" align="center">
         <?php include("../partialView/top.php"); ?>
@@ -41,33 +15,39 @@
             <td align="center">
 
                 <br><br>
-                
+
                 <input type="text" id="categorySearch"  onkeyup="searchCategory()" placeholder="Search category.." size="60">
-				
+
                 <br><br>
 
 
                 <table id="categoryTable" align="center" width="80%" cellspacing="20px" cellpadding="10px">
-                    <tr height="60px">
-                        <td align="center" bgcolor="#ddd">
+                    <tr height="60px" >
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
                                 <font color="#555" size="4"> <b>Literature & Fiction</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
                                 <font color="#555" size="4"> <b>Romance</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
                                 <font color="#555" size="4"> <b>Crime</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
+                            <a href="#">
+                                <img src="../../img/book/booklogo.png" width="55px"> <br>
+                                <font color="#555" size="4"> <b>Thriller</b> </font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
                                 <font color="#555" size="4"> <b>Thriller</b> </font>
@@ -75,87 +55,73 @@
                         </td>
 
                     </tr>
-                    <tr height="60px">
-                        <td align="center" bgcolor="#ddd">
+                    <tr height="60px" >
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Children & Young Adult</b> </font>
+                                <font color="#555" size="4"> <b>Literature & Fiction</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Sci-FI</b> </font>
+                                <font color="#555" size="4"> <b>Romance</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Fantasy</b> </font>
+                                <font color="#555" size="4"> <b>Crime</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Buisness $ Economics</b> </font>
+                                <font color="#555" size="4"> <b>Thriller</b> </font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd" width="20%">
+                            <a href="#">
+                                <img src="../../img/book/booklogo.png" width="55px"> <br>
+                                <font color="#555" size="4"> <b>Thriller</b> </font>
                             </a>
                         </td>
 
                     </tr>
-                    <tr height="60px">
-                        <td align="center" bgcolor="#ddd">
+                    <tr height="60px" >
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Biographies</b> </font>
+                                <font color="#555" size="4"> <b>Literature & Fiction</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>TextBook</b> </font>
+                                <font color="#555" size="4"> <b>Romance</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
                                 <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>ART</b> </font>
+                                <font color="#555" size="4"> <b>Crime</b> </font>
                             </a>
                         </td>
-                        <td align="center" bgcolor="#ddd">
+                        <td align="center" bgcolor="#ddd" width="20%">
                             <a href="#">
-                                <img src="../../img/book/booklogo.png" width="20%"> <br>
-                                <font color="#555" size="4"> <b>Translate Book</b> </font>
+                                <img src="../../img/book/booklogo.png" width="55px"> <br>
+                                <font color="#555" size="4"> <b>Thriller</b> </font>
+                            </a>
+                        </td>
+                        <td align="center" bgcolor="#ddd" width="20%">
+                            <a href="#">
+                                <img src="../../img/book/booklogo.png" width="55px"> <br>
+                                <font color="#555" size="4"> <b>Thriller</b> </font>
                             </a>
                         </td>
 
-                  <!--  </tr>
-                    <tr height="60px">
-                        <td align="center" bgcolor="#ddd">
-                            <a href="#">
-                                <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Science Fiction</b> </font>
-                            </a>
-                        </td>
-                        <td align="center" bgcolor="#ddd">
-                            <a href="#">
-                                <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Science Fiction</b> </font>
-                            </a>
-                        </td>
-                        <td align="center" bgcolor="#ddd">
-                            <a href="#">
-                                <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Science Fiction</b> </font>
-                            </a>
-                        </td>
-                        <td align="center" bgcolor="#ddd">
-                            <a href="#">
-                                <img src="../../img/book/booklogo.png" width="55px"> <br>
-                                <font color="#555" size="4"> <b>Science Fiction</b> </font>
-                            </a>
-                        </td>
+                    </tr>
 
-                    </tr> -->
 
 
                 </table>
--- view/partialView/advanceSearch.php
+++ view/partialView/advanceSearch.php
@@ -1,53 +1,6 @@
+
 <br><br>
 
-
-<<<<<<< HEAD
-Book Name: <input name="searchAuthor" >
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-Category:
-<select>
-  <option value="volvo"></option>
-  <option value="volvo">Literature & Fiction</option>
-  <option value="volvo">Romance</option>
-  <option value="volvo">Crime</option>
-  <option value="volvo">Thriller</option>
-  <option value="volvo">Children & Young Adult</option>
-  <option value="volvo">Sci-FI</option>
-  <option value="volvo">Fantasy</option>
-  <option value="volvo">Buisness $ Economics</option>
-  <option value="volvo">Biographies</option>
-  <option value="volvo">TextBook</option>
-  <option value="volvo">ART</option>
-		
-</select>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-Publication:
-<select>
-    <option value="volvo"></option>
-    <option value="volvo">Bangla Academy</option>
-    <option value="volvo">Onno Prokash</option>
-    <option value="volvo">Somoy Prokashon</option>
-	<option value="volvo">Anondo Prokashon</option>
-	<option value="volvo">Onnesha Prokasoni</option>
-	<option value="volvo">Priyarshon</option>
-	<option value="volvo">Suchi Potro</option>
-    <option value="volvo">Prothoma Prokasoni</option>
-</select>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-Author: 
-<select>
-  <option value="volvo"></option>
-  <option value="volvo">MD Jafor Iqbal</option>
-  <option value="volvo">Humayon Ahmed</option>
-  <option value="volvo">Anisul Haque</option>
-  <option value="volvo">Humayon Azad</option>
-  <option value="volvo">Noor Hassan</option>
-</select>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-</br> </br>
-<input type="submit" value=" Advance Search ">
-<br><br>
-=======
 Book Name: <input name="name" >
 Category: <input name="caegory" >
 Publication: <input name="publication" >
@@ -56,4 +9,3 @@ Author: <input name="author" >
 <input type="submit" value=" Advance Search ">
 <br><br>
 <hr width="95%" color="white">
->>>>>>> 63fa62b80ba793d615bed9824629453ae9a8138d
