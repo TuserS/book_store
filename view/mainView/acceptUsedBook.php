250338c997022543a7af82986a901756d32fa26a
Md Asifur Rahman <asif0531@live.com>
2017-12-21 13:02:46 +0600

    used book updated

---
+++ view/mainView/acceptUsedBook.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <title>Contact Us</title>
+</head>
+<body>
+<font size="2" face="Consolas" color="SlateGrey">
+    <table action="#"  width="95%" align="center">
+        <?php include("../partialView/top.php"); ?>
+        <tr>
+            <td>
+                <table>
+
+                    <tr valign="top" height="500px">
+                        <?php include("../partialView/nav.php"); ?>
+                        <td bgcolor="WhiteSmoke" width="80%" valign="top">
+
+
+                            <table align="center" width="70%">
+                                <td align="center">
+                                    <br><br>
+                                    <font size="5" color="LightSalmon"><b> Accept Used Books </b></font>
+
+                                    <br><br>
+                                    <input name="searchBook" >
+                                    <input type="submit" value=" Search Used Books ">
+                                    <br><br>
+
+                                    <!-- Informaton table -->
+                                    <table align="center"  width="70%" id="cartTable">
+                                        <tr bgcolor="#bbb" height="40px" align="center" >
+                                            <td><b>#</b></td>
+                                            <td><b>Cover</b></td>
+                                            <td><b>Name</b></td>
+                                            <td><b>Writer</b></td>
+                                            <td><b>Price</b></td>
+                                            <td><b>New Price</b></td>
+                                            <td><b>Request</b></td>
+                                        </tr>
+                                        <tr bgcolor="#ddd" align="center" height="50px">
+                                            <td>1</td>
+                                            <td><img src="../../img/book/book1.jpg" width="30px"></td>
+                                            <td><a href="bookDetails.php">Book Name 1</a> </td>
+                                            <td><a href="authorDetails.php">Write Name 1</a></td>
+                                            <td>200 Tk</td>
+                                            <td>80 Tk</td>
+                                            <td> <input type="submit" value=" Accept "> </td>
+                                        </tr>
+                                        <tr bgcolor="#ddd" align="center" height="50px">
+                                            <td>2</td>
+                                            <td><img src="../../img/book/book1.jpg" width="30px"></td>
+                                            <td><a href="bookDetails.php">Book Name 2</a> </td>
+                                            <td><a href="authorDetails.php">Write Name 2</a></td>
+                                            <td>250 Tk</td>
+                                            <td>110 Tk</td>
+                                            <td> <input type="submit" value=" Accept "> </td>
+                                        </tr>
+                                        <tr bgcolor="#ddd" align="center" height="50px">
+                                            <td>3</td>
+                                            <td><img src="../../img/book/book1.jpg" width="30px"></td>
+                                            <td><a href="bookDetails.php">Book Name 3</a> </td>
+                                            <td><a href="authorDetails.php">Write Name 3</a></td>
+                                            <td>460 Tk</td>
+                                            <td>200 Tk</td>
+                                            <td> <input type="submit" value=" Accept "> </td>
+                                        </tr>
+
+                                    </table>
+                                    <!-- Information table close -->
+
+                                </td>
+                            </table>
+                            <br><br>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <?php include("../partialView/bottom.php"); ?>
+    </table>
+</font>
+</body>
+</html>
