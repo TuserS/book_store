02d9e8eaecec31673b1192ca49eaed5078d603af
Md Asifur Rahman <asif0531@live.com>
2017-12-21 13:31:20 +0600

    donation updated

---
+++ view/mainView/acceptDonation.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+    <title>Contact Us</title>
+</head>
+<body>
+<font size="2" face="Consolas" color="SlateGrey">
+    <table action="#"  width="95%" align="center">
+        <?php include("../partialView/top.php"); ?>
+        <tr>
+            <td>
+                <table>
+
+                    <tr valign="top" height="500px">
+                        <?php include("../partialView/nav.php"); ?>
+                        <td bgcolor="WhiteSmoke" width="80%" valign="top">
+
+
+                            <table align="center" width="95%">
+                                <td align="center">
+                                    <br><br>
+                                    <font size="5" color="LightSalmon"><b> Accept Donation </b></font>
+
+                                    <br><br>
+                                    <input name="searchBook" >
+                                    <input type="submit" value=" Search organization ">
+                                    <br><br>
+
+                                    <!-- Informaton table -->
+                                    <table align="center"  width="95%" id="#">
+                                        <tr bgcolor="#bbb" height="40px" align="center" >
+                                            <td width="5%"><b>#</b></td>
+                                            <td width="20%"><b>Organization Name</b></td>
+                                            <td width="65%"><b>Reason</b></td>
+                                            <td width="10%"><b>Request</b></td>
+                                        </tr>
+                                        <tr bgcolor="#ddd" align="center" height="50px">
+                                            <td>1</td>
+                                            <td>Organization Name 1</td>
+                                            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis recusandae excepturi eos, inventore maiores officia,
+                                                ex reiciendis nisi velit, quam pariatur reprehenderit molestiae repellendus quaerat. Maiores, amet recusandae commodi
+                                                provident animi magni eligendi nesciunt molestias voluptatem distinctio ab corporis, quos eum. Enim eligendi, laborum,
+                                                labore aliquam ex facilis ab molestiae.</td>
+                                            <td>
+                                                <input type="submit" value="  Accept  "><br><br>
+                                                <input type="submit" value=" Remove ">
+                                            </td>
+                                        </tr>
+                                        <tr bgcolor="#ddd" align="center" height="50px">
+                                            <td>1</td>
+                                            <td>Organization Name 1</td>
+                                            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis recusandae excepturi eos, inventore maiores officia,
+                                                ex reiciendis nisi velit, quam pariatur reprehenderit molestiae repellendus quaerat. Maiores, amet recusandae commodi
+                                                provident animi magni eligendi nesciunt molestias voluptatem distinctio ab corporis, quos eum. Enim eligendi, laborum,
+                                                labore aliquam ex facilis ab molestiae.</td>
+                                            <td>
+                                                <input type="submit" value="  Accept  "><br><br>
+                                                <input type="submit" value=" Remove ">
+                                            </td>
+                                        </tr>
+                                        <tr bgcolor="#ddd" align="center" height="50px">
+                                            <td>1</td>
+                                            <td>Organization Name 1</td>
+                                            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis recusandae excepturi eos, inventore maiores officia,
+                                                ex reiciendis nisi velit, quam pariatur reprehenderit molestiae repellendus quaerat. Maiores, amet recusandae commodi
+                                                provident animi magni eligendi nesciunt molestias voluptatem distinctio ab corporis, quos eum. Enim eligendi, laborum,
+                                                labore aliquam ex facilis ab molestiae.</td>
+                                            <td>
+                                                <input type="submit" value="  Accept  "><br><br>
+                                                <input type="submit" value=" Remove ">
+                                            </td>
+                                        </tr>
+
+
+                                    </table>
+                                    <!-- Information table close -->
+
+                                </td>
+                            </table>
+                            <br><br>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <?php include("../partialView/bottom.php"); ?>
+    </table>
+</font>
+</body>
+</html>
